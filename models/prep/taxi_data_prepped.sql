71ab5b7d6c4e5d63231a694fc7c82afffd7fcb2a
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-06-12 12:38:42 +0200

    s

--- models/prep/taxi_data_prepped.sql
+++ models/prep/taxi_data_prepped.sql
@@ -4,7 +4,7 @@
 WITH taxi_reordered AS (
     SELECT * ,
     TIMESTAMP_DIFF(dropoff_datetime, pickup_datetime, SECOND) AS trip_duration_seconds,
-    ROUND(TIMESTAMP_DIFF(dropoff_datetime, pickup_datetime, SECOND) / 60.0, 4) AS trip_duration_minutes
+    ROUND(TIMESTAMP_DIFF(dropoff_datetime, pickup_datetime, SECOND) / 60.0, 4) AS trip_duration_minutes_exact
     FROM {{ref('stg_taxi_data')}}
 )
 SELECT * 
