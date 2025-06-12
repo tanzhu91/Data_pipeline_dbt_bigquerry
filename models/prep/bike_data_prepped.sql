7d7ea6835d6c778e8903e1dc02d899e12552b25f
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-06-12 12:21:16 +0200

    s

--- models/prep/bike_data_prepped.sql
+++ models/prep/bike_data_prepped.sql
@@ -3,7 +3,7 @@
 
 WITH bike_reordered AS (
     SELECT * ,
-    DATETIME_DIFF(starttime, stoptime, MINUTE) AS trip_duration_minutes
+    DATETIME_DIFF(stoptime, starttime, MINUTE) AS trip_duration_minutes
     FROM {{ref('stg_citibike_trips')}}
 )
 SELECT * 
