9ee423ff8bb1e9f27724571a72a0094272421465
Tanzhu Dereli <tanzhu.dereli91@gmail.com>
2025-06-14 22:27:16 +0200

    s

--- models/staging/stg_taxi_data_green.sql
+++ models/staging/stg_taxi_data_green.sql
@@ -1,5 +1,4 @@
 {{ config(materialized='view') }}
 
--- This is your raw, materialized table
 select *
-from {{ source('new_york_taxi_trips', 'tlc_green_trips_2022') }}
+from {{ source('nyc_taxi', 'tlc_green_trips_2022') }}
